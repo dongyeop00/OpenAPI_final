39e236a4f23f5b1be886b892d226f7c36fe7c169
dongyeop00 <ehd_duq94@naver.com>
2024-06-02 04:05:25 +0900

    css

--- src/main/java/com/dongyeop/dbp_api/Main/Controller/MainController.java
+++ src/main/java/com/dongyeop/dbp_api/Main/Controller/MainController.java
@@ -18,30 +18,41 @@ public class MainController {
     private final ProductService productService;
 
     @GetMapping("/asd")
-    public String form(Model model){
+    public String form(Model model) {
         List<ProductEntity> products = productService.getAllProducts();
         System.out.println(products);
-        model.addAttribute("products",products);
+        model.addAttribute("products", products);
         return "index";
     }
 
     @GetMapping("/")
-    public String main(){
+    public String main() {
         return "main";
     }
 
     @GetMapping("/search")
     public String searchText(@RequestParam(name = "filter") String filter,
-                         @RequestParam(name = "query") String query,
-                         Model model){
-        System.out.println(filter + " " +query);
-        List<ProductEntity> productEntities = productService.getQueryProducts(filter,query);
-        model.addAttribute("products",productEntities);
+                             @RequestParam(name = "query") String query,
+                             Model model) {
+        System.out.println(filter + " " + query);
+        List<ProductEntity> productEntities = productService.getQueryProducts(filter, query);
+        model.addAttribute("products", productEntities);
         return "index";
     }
 
     @PostMapping("/submit")
-    public String searchButton(@RequestParam(name = "options", required = false) List<String> options){
+    public String searchButton(@RequestParam(name = "options", required = false) List<String> options) {
         return null;
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "check";
+    }
+
+    @GetMapping("/slide")
+    public String slide() {
+        return "slide";
+
     }
 }
